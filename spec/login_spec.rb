3a8904c3ff2da1c146df287b2fcf73e37e23f27b
Bill Creager <billc@spiceworks.com>
2015-12-15 21:38:54 -0600

    step_two

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -13,4 +13,18 @@ describe "Login" do
     expect(@browser.url).to eq('http://localhost:4567/login')
     @browser.close
   end
+
+  it "should allow login with valid user and password" do
+    @browser = Watir::Browser.new :chrome
+    width = @browser.execute_script('return screen.width;')
+    height = @browser.execute_script('return screen.height;')
+    @browser.driver.manage.window.move_to(0, 0)
+    @browser.driver.manage.window.resize_to(width, height)
+    @browser.goto('localhost:4567/login')
+    @browser.input(id: 'inputEmail').send_keys('test@test.com')
+    @browser.input(id: 'inputPassword').send_keys('password')
+    @browser.button(id: 'login').click
+    expect(@browser.url).to eq('http://localhost:4567/table?')
+    @browser.close
+  end
 end
