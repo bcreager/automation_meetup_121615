19a7b3bc6d307f4b38eb4c2570b66b9fbc5b64d6
Bill Creager <billc@spiceworks.com>
2015-12-16 08:55:40 -0600

    step_five

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -1,16 +1,6 @@
 require_relative '../spec_helper'
-require 'watir-webdriver'
 
 describe "Login" do
-  before(:each) do
-    @browser = Watir::Browser.new :chrome
-    width = @browser.execute_script('return screen.width;')
-    height = @browser.execute_script('return screen.height;')
-    @browser.driver.manage.window.move_to(0, 0)
-    @browser.driver.manage.window.resize_to(width, height)
-    @browser.goto('localhost:4567/login')
-  end
-
   it "should require login" do
     @browser.button(id: 'login').click
     expect(@browser.url).to eq('http://localhost:4567/login')
@@ -21,9 +11,5 @@ describe "Login" do
     @browser.input(id: 'inputPassword').send_keys('password')
     @browser.button(id: 'login').click
     expect(@browser.url).to eq('http://localhost:4567/table?')
-  end
-
-  after(:each) do
-    @browser.close
   end
 end
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -1,14 +1,7 @@
 require_relative '../spec_helper'
-require 'watir-webdriver'
 
 describe "Table" do
   before(:each) do
-    @browser = Watir::Browser.new :chrome
-    width = @browser.execute_script('return screen.width;')
-    height = @browser.execute_script('return screen.height;')
-    @browser.driver.manage.window.move_to(0, 0)
-    @browser.driver.manage.window.resize_to(width, height)
-    @browser.goto('localhost:4567/login')
     @browser.input(id: 'inputEmail').send_keys('test@test.com')
     @browser.input(id: 'inputPassword').send_keys('password')
     @browser.button(id: 'login').click
@@ -17,9 +10,5 @@ describe "Table" do
   it "should show table data on table page" do
     rows = @browser.trs
     expect(rows.count).to eq(10)
-  end
-
-  after(:each) do
-    @browser.close
   end
 end
